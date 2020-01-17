456f44ad3822fd92cf6945b68d294544f8f6ccc5
Jose Luis Nabor Bartolo <jose.nabor@nearshoretechnology.com>
2020-01-17 15:17:51 -0600

    test odepipe

---
+++ slnTestAWS/AWSServerless1/Controllers/YoMerenguesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWSServerless1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YoMerenguesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "hola guapisimo" };
+        }
+    }
+}
